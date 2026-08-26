a289c5f76774def4172af0e87acc5669071a632c
gabrielhono <gabrielhono.o@gmail.com>
2026-08-26 20:26:06 +0200

    feat: request location permission on GoogleMapScreen

--- app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapScreen.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapScreen.kt
@@ -1,10 +1,20 @@
 package com.unicofrance.uniexo.ui.googleMap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.maps.android.compose.GoogleMap
+import androidx.compose.runtime.setValue
 
 @Composable
 fun GoogleMapScreen(
@@ -12,6 +22,29 @@ fun GoogleMapScreen(
     viewModel: GoogleMapViewModel
 ) {
     val location by viewModel.location.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
-    GoogleMap(modifier = modifier)
+    var hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            hasLocationPermission = isGranted
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        if (!hasLocationPermission) {
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    GoogleMap (modifier = modifier)
 }
